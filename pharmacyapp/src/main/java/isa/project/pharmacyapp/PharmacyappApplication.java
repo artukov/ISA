30ad19f3c4b9b407223c214a3461b0cb6c0d72a5
gitSrdzaan <srdjan_kralj_97@live.com>
2020-11-09 11:11:04 +0100

    backend spring boot setup and creation

---
+++ pharmacyapp/src/main/java/isa/project/pharmacyapp/PharmacyappApplication.java
@@ -0,0 +1,13 @@
+package isa.project.pharmacyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyappApplication.class, args);
+	}
+
+}
---
+++ pharmacyapp/src/test/java/isa/project/pharmacyapp/PharmacyappApplicationTests.java
@@ -0,0 +1,13 @@
+package isa.project.pharmacyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
