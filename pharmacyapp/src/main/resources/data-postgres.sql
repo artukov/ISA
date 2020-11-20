0e8ef7ec4da4fd64c65263facba238e17e3335d0
gitSrdzaan <srdjan_kralj_97@live.com>
2020-11-20 20:11:12 +0100

    correcting spelling in data-postgres.sql

--- pharmacyapp/src/main/resources/data-postgres.sql
+++ pharmacyapp/src/main/resources/data-postgres.sql
@@ -1,3 +1,3 @@
 
-insert into address(id, street, street_number, city, country) values (2,'street', 15,'city','country');
+insert into public.address(id, street, street_number, city, country) values (2,'street', 15,'city','country');
 
--- pharmacyapp/src/test/java/isa/project/pharmacyapp/PharmacyappApplicationTests.java
+++ pharmacyapp/src/test/java/isa/project/pharmacyapp/PharmacyappApplicationTests.java
@@ -1,11 +1,11 @@
 package isa.project.pharmacyapp;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
 
-@RunWith(SpringRunner.class)
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
 @SpringBootTest
 public class PharmacyappApplicationTests {
 
