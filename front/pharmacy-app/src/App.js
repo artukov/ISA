d8eb1d0440a213f1e4751a6d4a752b8c91658188
panta246 <stefan.pantic@live.com>
2021-01-11 23:36:39 +0100

    homepage

--- front/pharmacy-app/src/App.js
+++ front/pharmacy-app/src/App.js
@@ -1,21 +1,24 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Protected } from 'react-router-dom';
 import './css/global.css';
 
 import HomePage from './components/homePage';
 import LoginPage from './components/loginPage';
 import RegistrationPage from './components/registrationPage';
 import PharmacyComponent from './components/pharmacy/PharmacyComponent';
+import UserProfile from './components/view/userProfile'
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path='/login' exact component={LoginPage} />
-        <Route path='/home' exact component={HomePage} />
         <Route path='/registration' exact component={RegistrationPage} />
+
+        <Route path='/home' exact component={HomePage} />
+
         <Route path='/pharmacy' exact component={PharmacyComponent}></Route>
         <Route path='/'>404 not here sry</Route>
-       
+
       </Switch>
     </Router>
   );
--- front/pharmacy-app/src/components/homePage.js
+++ front/pharmacy-app/src/components/homePage.js
@@ -1,78 +1,46 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, Form, Button, NavDropdown, FormControl } from 'react-bootstrap'
+import { Button, Tab, Tabs, Container, Row, Col } from 'react-bootstrap'
 
 import axiosConfig from '../config/AxiosConfig';
+import UserProfile from '../components/view/userProfile';
+import Drugstore from "../components/view/drugstore";
+import Medicaments from '../components/view/medicaments';
 
 class homePage extends Component {
-    /* constructor(props) {
-        super(props);
-    } */
 
-    /* componentWillMount() {
+    getCurrent = () => {
 
-    }
-
-    
-
-    componentWillReceiveProps(nextProps) {
-
-    }
-
-  
-
-    componentWillUpdate(nextProps, nextState) {
-
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-
-    }
-
-    componentWillUnmount() {
-
-    } */
-
-    shouldComponentUpdate(nextProps, nextState) {
-        
-    }
-
-    componentDidMount() {
-      
-    }
-
-    getCurrent = () =>{
-        
         axiosConfig.post('/user/current')
-        .then(res => console.log(res))
-        .catch(err => console.log(err));
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
     }
 
     render() {
         return (
-            <div className='navbar'>
-                <Navbar bg="light" expand="lg">
-                    <Navbar.Brand href="/home">App name</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="#home">Profil</Nav.Link>
-                            <Nav.Link href="#link">Apoteka</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                        <Form inline>
-                            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                            <Button variant="primary">Search</Button>
-                        </Form>
-                    </Navbar.Collapse>
-                </Navbar>
-                <Button onClick = {() => this.getCurrent}>Get current user</Button>
-                <Button onClick = {() => window.location = '/pharmacy'}>Pharmacy</Button>
+            <div>
+                <Container size='xl'>
+                    <Row>
+                        <Col sm={12}>
+                            <Tabs defaultActiveKey="drugstores"
+                                id="controlled-tab-example">
+
+                                <Tab eventKey="user" title="User">
+                                    <UserProfile />
+                                </Tab>
+                                <Tab eventKey="drugstores" title="Drugstores" >
+                                    <Drugstore />
+                                </Tab>
+                                <Tab eventKey="medicaments" title="Medicaments" >
+                                    <Medicaments />
+                                </Tab>
+                            </Tabs>
+                        </Col>
+
+                    </Row>
+                </Container>
+
+                <Button onClick={() => this.getCurrent()}>Get current user</Button>
+                <Button onClick={() => window.location = '/pharmacy'}>Pharmacy</Button>
             </div>
         );
     }
