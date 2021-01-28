d8ce3263815866b1466eb3150b1376ff9fbac807
Artukov <nikolartukov@gmail.com>
2021-01-28 01:02:26 +0100

    ADD CrossOrigin to absenceRequestController

--- pharmacyapp/src/main/java/isa/project/pharmacyapp/controller/AbsenceRequestController.java
+++ pharmacyapp/src/main/java/isa/project/pharmacyapp/controller/AbsenceRequestController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "/absence")
 public class AbsenceRequestController {
 
