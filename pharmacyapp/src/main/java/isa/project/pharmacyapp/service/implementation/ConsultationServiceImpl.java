92891db58826b25d9cb5c3760fa56e4f4815f72b
Artukov <nikolartukov@gmail.com>
2021-01-29 18:35:28 +0100

    ADD new consultation in service

--- pharmacyapp/src/main/java/isa/project/pharmacyapp/service/implementation/ConsultationServiceImpl.java
+++ pharmacyapp/src/main/java/isa/project/pharmacyapp/service/implementation/ConsultationServiceImpl.java
@@ -3,19 +3,15 @@ package isa.project.pharmacyapp.service.implementation;
 import isa.project.pharmacyapp.dto.AppointmentDTO;
 import isa.project.pharmacyapp.dto.ConsultationDTO;
 
+import isa.project.pharmacyapp.dto.DermatologistDTO;
 import isa.project.pharmacyapp.dto.WorkingHoursDTO;
 import isa.project.pharmacyapp.exception.InsertingConsultationException;
 import isa.project.pharmacyapp.exception.InsertingDermatologistException;
-import isa.project.pharmacyapp.model.Calendar;
-import isa.project.pharmacyapp.model.Consultation;
-import isa.project.pharmacyapp.model.Drug;
-import isa.project.pharmacyapp.model.Patient;
-import isa.project.pharmacyapp.model.Pharmacist;
+import isa.project.pharmacyapp.model.*;
 import isa.project.pharmacyapp.repository.ConsultationRepository;
 import isa.project.pharmacyapp.repository.DrugRepository;
 import isa.project.pharmacyapp.repository.PatientRepository;
 import isa.project.pharmacyapp.repository.PharmacistRepository;
-import isa.project.pharmacyapp.model.Pharmacy;
 import isa.project.pharmacyapp.model.embedded_ids.CalendarAppointmentsID;
 import isa.project.pharmacyapp.model.many2many.CalendarAppointments;
 import isa.project.pharmacyapp.repository.*;
@@ -27,6 +23,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class ConsultationServiceImpl implements ConsultationService {
@@ -111,8 +108,23 @@ public class ConsultationServiceImpl implements ConsultationService {
         catch (Exception e){
             throw new Exception();
         }
+    }
 
+    @Override
+    public void modifyConsultation(Long consultationId, ConsultationDTO consultationDTO) throws Exception {
+        Consultation consultation = consultationRepository.findById(consultationId).orElse(null);
 
+        if(consultation == null){
+            throw new NoSuchElementException("ConsultationSerivceImpl::modifyConsultation(Long id, ConsultationDTO consultationDTO)" +
+                    "consultation could not be find by the given id");
+        }
 
+        try {
+            this.saveConsultation(consultation, consultationDTO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception("ConsultationServiceImpl::modifyConsultation(Long id, ConsultationDTO consultationDTO)" +
+                    "saving of the modified object did not excecute");
+        }
     }
 }
